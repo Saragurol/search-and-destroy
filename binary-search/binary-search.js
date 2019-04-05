97aafa2cafd8ed7bce88f283634ca84b40838810
Linda Saraguro <lindasaraguro@yahoo.com>
2019-04-05 12:28:13 -0400

    completed BST

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,27 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
+	array.sort((a, b) => a-b)
 	
+	let midpt = Math.floor((array.length)/2)
+	let arr1 = array.slice(0, midpt)
+	let arr2 = array.slice(midpt)
+	if (target >=arr2[0]){
+		for(let j=0; j < arr2.length; j++){
+			if(arr2[j] === target){
+				return true
+			}
+		}
+	}
+	else{
+		console.log("FIRST ELEMT", arr1[arr1.length-1])
+		for(let i=0; i < arr1.length; i++){
+			if(arr1[i] === target){
+				return true
+			}
+		}
+	}
+	return false
 };
 
 /*
